0e8ccbaffce9a66920d3d765ebcfade246840b17
Lakshay Pareek <126066065+lakpareek@users.noreply.github.com>
2024-07-31 12:42:12 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -18,12 +18,8 @@ app.use((err, req, res, next) => {
   res.status(500).json({ message: "An internal server error occurred" });
 });
 const pool = new Pool({
-  user: process.env.PG_USER,
-  host: process.env.PG_HOST,
-  database: process.env.PG_DATABASE,
-  password: process.env.PG_PASSWORD,
-  port: process.env.PG_PORT,
-});
+  connectionString: process.env.POSTGRES_URL,
+})
 
 const initializeDB = async () => {
     try {
